105a0e5212f34e8e98efc30dddeea68be0d261de
Andreas <andreaszechmeister@chello.at>
2020-09-12 15:50:44 +0200

    added script logic

--- Assets/Scenes/NewBehaviourScript01.cs
+++ Assets/Scenes/NewBehaviourScript01.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class NewBehaviourScript01 : MonoBehaviour
 {
+    public bool testbool1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        testbool1 = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        testbool1 = true;
     }
 }
